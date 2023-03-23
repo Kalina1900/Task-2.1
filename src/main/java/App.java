b9ff2e76e59cbd34731835a8d6a9e0458a1088c0
Kalina1900 <8952737594dva@mail.ru>
2023-03-23 15:32:43 +0500

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,9 +4,20 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+                new AnnotationConfigApplicationContext(AppConfig.class);          // создаём сам контекст
         HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+                applicationContext.getBean("helloworld", HelloWorld.class);       /////
+        HelloWorld bean1 =                                                             //
+                applicationContext.getBean("helloworld", HelloWorld.class);         //
+        Cat bean2 =                                                                    //  Вызываем бины
+                applicationContext.getBean("cat", Cat.class);                       //
+        Cat bean3 =                                                                    //
+                applicationContext.getBean("cat", Cat.class);                     ////
+        boolean comparison = bean==bean1;
+        boolean comparison2 = bean2==bean3;
+        System.out.println(comparison);
+        System.out.println(comparison2);
+
+
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-@Configuration
+@Configuration              // Настроечный класс
 public class AppConfig {
  
-    @Bean(name="helloworld")
-    public HelloWorld getHelloWorld() {
-        HelloWorld helloWorld = new HelloWorld();
+    @Bean(name="helloworld")                              //содержится метод, помеченный аннотацией как бин,
+    public HelloWorld getHelloWorld() {                   // этот метод будет выполнен при запуске и его результат
+        HelloWorld helloWorld = new HelloWorld();         // станет объектом, управляемым Спрингом.
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCat(){
+        Cat cat = new Cat();
+        cat.setMessage("Cat!");
+        return cat;
+    }
+
 }
